b08c74f20dd57849984d676aba1916db234c39f1
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-07 10:41:37 -0700

    Zombie constructor inheriting methods from Animal constructor

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//animals and zombies
-//constructor
+//animals and zombies attacking each other game
+//Animal constructor has all the methods and Zombie is inheriting those methods from Animal constructor
 function Animal (name, ability, armor, strengthIndex){
   this.name = name
   this.ability = ability
@@ -27,13 +27,36 @@ function Animal (name, ability, armor, strengthIndex){
 
 
 Animal.prototype.attack = function(enemy){
-  if(this.strength[this.strengthIndex].dexterity === 50){
-    console.log(this.name + ' attacked the opponent in his butt using his ' + this.ability + ' ! Gave ' + enemy.name + ' a fatal ' + this.strength[this.strengthIndex].damage + ' damage!!')
-    enemy.damageTaken(this.strength[this.strengthIndex].damage)
+  var dext = this.strength[this.strengthIndex].dexterity
+  var dmg = this.strength[this.strengthIndex].damage;
+  if(dext === 50){
+    console.log(this.name + ' attacked the opponent in his neck using his ' + this.ability + ' ! Gave ' + enemy.name + ' a fatal ' + dmg + ' damage!!')
+    enemy.damageTaken(dmg);
     return;
   }
-   else if(this.strength[this.strengthIndex].dexterity >= 40 && this.strength[this.strengthIndex].dexterity <= 49){
-    console.log(this.name + ' attacked the opponent in his butt using his ' + this.ability + ' !! Gave ' + enemy.name + ' some damage. Pretty good damage of ' + this.strength[this.strengthIndex].damage + ' damage!');
+   else if(dext >= 40 && dext <= 49){
+    console.log(this.name + ' attacked the opponent in his butt using his ' + this.ability + ' !! Gave ' + enemy.name + ' some damage. Pretty good damage of ' + dmg + ' damage!');
+    enemy.damageTaken(dmg);
+    console.log(enemy.name + ' now has ' + enemy.hp + ' HP.');
+    return;
+  }
+  else if(dext >= 30 && dext <= 39){
+    console.log(this.name + ' attacked the opponent in his back using his ' + this.ability + ' ! Gave ' + enemy.name + ' enough damage. ' + dmg + ' damage!');
+    enemy.damageTaken(dmg);
+    console.log(enemy.name + ' now has ' + enemy.hp + ' HP.');
+    return;
+  }
+
+  else if(dext >= 20 && dext <= 29){
+    console.log(this.name + ' attacked the opponent in his shin using his ' + this.ability + ' ! Gave ' + enemy.name + ' enough damage. ' + dmg + ' damage!');
+    enemy.damageTaken(dmg);
+    console.log(enemy.name + ' now has ' + enemy.hp + ' HP.');
+    return;
+  }
+  else if(dext >= 10 && dext <= 19){
+    console.log(this.name + ' attacked the opponent in his shin using his ' + this.ability + ' ! Gave ' + enemy.name + ' enough damage. ' + dmg + ' damage!');
+    enemy.damageTaken(dmg);
+    console.log(enemy.name + ' now has ' + enemy.hp + ' HP.');
     return;
   }
 }
@@ -57,16 +80,24 @@ function Zombie (name, ability, strengthIndex) {
   this.strengthIndex = strengthIndex;
 }
 
-Zombie.prototype.damageTaken = function(damage){
+Animal.prototype.damageTaken = function(damage){
+  this.strengthIndex -= 1
+  var dext = this.strength[this.strengthIndex].dexterity;
   this.hp -= damage;
   console.log('Zombie took a damage of ' + damage + ' and Zombie hp is at ' + this.hp + '.');
+  console.log(this.name + ' dexterity is now at ' + dext + '.')
 }
 
+//inheriting attack from Animal and damage from Zombie
+//both ways work!
+Zombie.prototype = Animal.prototype;
 // Zombie.prototype = new Animal();
-// Zombie.prototype.attack =
 
 var newRabbit = new Animal('Rabbit the Jumper', 'Super Kick', 'Fluffy Fur is like a air bag', 4);
 var zombie1 = new Zombie('Zombi1', 'acid reflects and spits out stomach acid', 2);
 console.log(newRabbit);
 console.log(zombie1);
+
+zombie1.attack(newRabbit);
+zombie1.attack(newRabbit);
 newRabbit.attack(zombie1);
